f6e8e731e737bbb3745419ba7c1f48b0b1e65eb8
Tom Hemery <tomhemery@outlook.com>
2023-08-12 11:53:18 +0100

    Add input form, no validation yet, css needs detablising

---
+++ Pages/QuotePersonalDetails.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MotivWebApp.Pages
+{
+    public class QuotePersonalDetailsModel : PageModel
+    {
+        private readonly ILogger<QuotePersonalDetailsModel> _logger;
+
+        public QuotePersonalDetailsModel(ILogger<QuotePersonalDetailsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
