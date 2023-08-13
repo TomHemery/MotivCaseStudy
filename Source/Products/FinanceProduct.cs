7e3bb830723d2228906b200f4519eb4fc7b265a1
Tom Hemery <tomhemery@outlook.com>
2023-08-13 17:05:20 +0100

    Spelling and a bit of fun

--- Source/Products/FinanceProduct.cs
+++ Source/Products/FinanceProduct.cs
@@ -6,13 +6,15 @@
         public readonly string description;
         public readonly float premium;
         public readonly int minimumScore;
+        public readonly string url;
 
-        public FinanceProduct(string name, string description, float premium, int minimumScore)
+        public FinanceProduct(string name, string description, float premium, int minimumScore, string url)
         {
             this.name = name;
             this.description = description; 
             this.premium = premium;
             this.minimumScore = minimumScore;
+            this.url = url;
         }
     }
 }
--- Source/Products/ProductRegistry.cs
+++ Source/Products/ProductRegistry.cs
@@ -7,10 +7,10 @@
         public ProductRegistry() 
         { 
             products = new List<FinanceProduct>{
-                new FinanceProduct("The Pigeon Plan", "A terrible plan that can barely fly", 100, 1),
-                new FinanceProduct("The Penguin Plan", "A plan that wobbles around from place to place, but it's good at swimming", 150, 2),
-                new FinanceProduct("The Peregrin Plan", "A plan that's really, really fast", 200, 3),
-                new FinanceProduct("The Pterodactly Plan", "A plan that has literally been around since the dinosaurs", 250, 4)
+                new FinanceProduct("The Pigeon Plan", "A terrible plan that can barely fly", 100, 1, "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcTTQEqKVXJ_3BSoRUNYNNWY_yZ0benAi36_pVKQad6_dMD1s3nEpD-2959xE-mKVqZdeXbfMC9HcNjw1cQ"),
+                new FinanceProduct("The Penguin Plan", "A plan that wobbles around from place to place, but it's good at swimming", 150, 2, "https://www.cabq.gov/artsculture/biopark/news/10-cool-facts-about-penguins/@@images/1a36b305-412d-405e-a38b-0947ce6709ba.jpeg"),
+                new FinanceProduct("The Peregrin Plan", "A plan that's really, really fast", 200, 3, "https://www.thebestofexmoor.co.uk/blog/wp-content/uploads/2023/02/peregrin-falcon-in-flight.jpeg"),
+                new FinanceProduct("The Pterodactyl Plan", "A plan that has literally been around since the dinosaurs", 250, 4, "https://carnegiemnh.org/wp-content/uploads/2016/04/ptero.jpg")
             };
         }
 
